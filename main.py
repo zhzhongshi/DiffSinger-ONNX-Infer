64adc00e60c31d14b62985c637d686fa10b840bb
zhzhongshi <39423408+zhzhongshi@users.noreply.github.com>
2023-11-22 23:40:26 +0800

    Update main.py add fastapi

--- main.py
+++ main.py
@@ -1,28 +1,90 @@
-from infer import Infer
+import importlib
+import re
+import os.path
+import uuid 
+import uvicorn
 
-from utils.hparams import set_hparams, hparams
+from pathlib import *
+from pydantic import BaseModel
+from typing import List
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from infer import Infer
+from utils.hparams import set_hparams
+from utils.hparams import hparams as hp
+import numpy as np
+import soundfile as sf
+
+app = FastAPI()
+
+class MainInfer:
+    def __init__(self, inference_cls="infer.Infer"):
+        pkg = ".".join(inference_cls.split(".")[:-1])
+        cls_name = inference_cls.split(".")[-1]
+        self.inference_cls = getattr(importlib.import_module(pkg), cls_name)
+
+    def greet(self, text: str, notes:str, notes_duration:str):
+        PUNCS = '。？；：'
+        sents = re.split(rf'([{PUNCS}])', text.replace('\n', ','))
+        sents_notes = re.split(rf'([{PUNCS}])', notes.replace('\n', ','))
+        sents_notes_dur = re.split(rf'([{PUNCS}])', notes_duration.replace('\n', ','))
+
+        if sents[-1] not in list(PUNCS):
+            sents = sents + ['']
+            sents_notes = sents_notes + ['']
+            sents_notes_dur = sents_notes_dur + ['']
+
+        audio_outs = []
+        s, n, n_dur = "", "", ""
+        for i in range(0, len(sents), 2):
+            if len(sents[i]) > 0:
+                s += sents[i] + sents[i + 1]
+                n += sents_notes[i] + sents_notes[i+1]
+                n_dur += sents_notes_dur[i] + sents_notes_dur[i+1]
+            if len(s) >= 400 or (i >= len(sents) - 2 and len(s) > 0):
+                audio_out = self.infer_ins.infer_once({
+                    'text': s,
+                    'notes': n,
+                    'notes_duration': n_dur,
+                })
+                audio_out = audio_out * 32767
+                audio_out = audio_out.astype(np.int16)
+                audio_outs.append(audio_out)
+                audio_outs.append(np.zeros(int(hp['audio_sample_rate'] * 0.3)).astype(np.int16))
+                s = ""
+                n = ""
+        audio_outs = np.concatenate(audio_outs)
+        return hp['audio_sample_rate'], audio_outs
+
+    def run(self,text,note,duration):
+        set_hparams("model/config.yaml")
+        infer_cls = self.inference_cls
+        self.infer_ins: Infer = infer_cls(hp)
+        sr, wav = self.greet(text, note, duration)
+        return sr,wav
+        
+class DataModel(BaseModel):
+    data: List[str]
+@app.post("/api/infer/")
+def inference(item: DataModel):
+    input=item.dict()
+    text=input["data"][0]
+    note=input["data"][1]
+    duration=input["data"][2]
+    g = MainInfer()
+    sr,wav=g.run(text,note,duration)
+    uuid1=uuid.uuid1()
+    if Path("cache/").exists():
+        sf.write(file=f'cache/{uuid1}.wav', data=wav, samplerate=sr)
+    else:
+        Path.mkdir(Path("cache/"))
+        sf.write(file=f'cache/{uuid1}.wav', data=wav, samplerate=sr)
+    return {"id":uuid1}
+
+@app.get("/api/getwav/",response_class=FileResponse)
+def getwav(uuid: str):
+    print(uuid)
+    return FileResponse(f"cache/{uuid}.wav",media_type="audio/wav")
 
 if __name__ == '__main__':
-    # debug
-    set_hparams('model/config.yaml')
-    a = Infer(hparams)
-    a.preprocess_input({'text': '你 说 你 不 SP 懂 为 何 在 这 时 牵 手 AP',
-                         'notes': 'D#4/Eb4 | D#4/Eb4 | D#4/Eb4 | D#4/Eb4 | rest | D#4/Eb4 | D4 | D4 | D4 | D#4/Eb4 | F4 | D#4/Eb4 | D4 | rest',
-                        'notes_duration': '0.113740 | 0.329060 | 0.287950 | 0.133480 | 0.150900 | 0.484730 | 0.242010 | 0.180820 | 0.343570 | 0.152050 | 0.266720 | 0.280310 | 0.633300 | 0.444590'
-                        })
-     
-
-    b = {
-        'text': '小酒窝长睫毛AP是你最美的记号',
-        'notes': 'C#4/Db4 | F#4/Gb4 | G#4/Ab4 | A#4/Bb4 F#4/Gb4 | F#4/Gb4 C#4/Db4 | C#4/Db4 | rest | C#4/Db4 | A#4/Bb4 | G#4/Ab4 | A#4/Bb4 | G#4/Ab4 | F4 | C#4/Db4',
-        'notes_duration': '0.407140 | 0.376190 | 0.242180 | 0.509550 0.183420 | 0.315400 0.235020 | 0.361660 | 0.223070 | 0.377270 | 0.340550 | 0.299620 | 0.344510 | 0.283770 | 0.323390 | 0.360340'
-    }
-    c = {
-        'text': '小酒窝长睫毛AP是你最美的记号',
-        'ph_seq': 'x iao j iu w o ch ang ang j ie ie m ao AP sh i n i z ui m ei d e j i h ao',
-        'note_seq': 'C#4/Db4 C#4/Db4 F#4/Gb4 F#4/Gb4 G#4/Ab4 G#4/Ab4 A#4/Bb4 A#4/Bb4 F#4/Gb4 F#4/Gb4 F#4/Gb4 C#4/Db4 C#4/Db4 C#4/Db4 rest C#4/Db4 C#4/Db4 A#4/Bb4 A#4/Bb4 G#4/Ab4 G#4/Ab4 A#4/Bb4 A#4/Bb4 G#4/Ab4 G#4/Ab4 F4 F4 C#4/Db4 C#4/Db4',
-        'note_dur_seq': '0.407140 0.407140 0.376190 0.376190 0.242180 0.242180 0.509550 0.509550 0.183420 0.315400 0.315400 0.235020 0.361660 0.361660 0.223070 0.377270 0.377270 0.340550 0.340550 0.299620 0.299620 0.344510 0.344510 0.283770 0.283770 0.323390 0.323390 0.360340 0.360340',
-        'is_slur_seq': '0 0 0 0 0 0 0 0 1 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0'
-    }  # input like Opencpop dataset.
-    a.preprocess_input(b)
-    a.preprocess_input(c, input_type='phoneme')
+    uvicorn.run(app)
